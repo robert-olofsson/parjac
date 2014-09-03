c29d92a5649599d8071349191cf45f3e2eac7b1a
Robert Olofsson <robo@khelekore.org>
2014-09-03 21:38:01 +0200

    Change the parser tests to work on tokens instead of string parsing.

--- test/org/khelekore/parjac/parser/TestParser.java
+++ test/org/khelekore/parjac/parser/TestParser.java
@@ -4,60 +4,84 @@ import java.nio.CharBuffer;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Locale;
 import org.khelekore.parjac.CompilerDiagnosticCollector;
-import org.khelekore.parjac.lexer.CharBufferLexer;
 import org.khelekore.parjac.lexer.Lexer;
+import org.khelekore.parjac.lexer.Token;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestParser {
     @Test
     public void testEmpty () {
-	testParse ("");
+	testParse ();
     }
 
     @Test
     public void testPackage () {
-	testParse ("package foo;");
-	testParseError ("package foo");
-	testParse ("package foo.bar;");
-	testParseError ("package foo.bar");
-	testParse ("@Foo package foo;");
-	testParse ("@Foo(a = \"hello\") package foo;");
+	// package foo
+	testParseError (Token.PACKAGE, Token.IDENTIFIER);
+	// package foo;
+	testParse (Token.PACKAGE, Token.IDENTIFIER, Token.SEMICOLON);
+	// package foo.bar
+	testParseError (Token.PACKAGE, Token.IDENTIFIER, Token.DOT, Token.IDENTIFIER);
+	// package foo.bar;
+	testParse (Token.PACKAGE, Token.IDENTIFIER, Token.DOT, Token.IDENTIFIER, Token.SEMICOLON);
+	// @Foo package foo;
+	testParse (Token.AT, Token.IDENTIFIER, Token.PACKAGE,
+		   Token.IDENTIFIER, Token.DOT, Token.IDENTIFIER, Token.SEMICOLON);
+	// @Foo(a = "hello") package foo;
+	testParse (Token.AT, Token.IDENTIFIER, Token.LEFT_PARANTHESIS, Token.IDENTIFIER,
+		   Token.EQUAL, Token.STRING_LITERAL, Token.RIGHT_PARANTHESIS,
+		   Token.PACKAGE, Token.IDENTIFIER, Token.SEMICOLON);
     }
 
     @Test
     public void testImport () {
-	testParse ("import foo;");
-	testParseError ("import foo");
-	testParse ("import foo.Bar;");
-	testParse ("import foo.*;");
-	testParse ("import foo.bar.*;");
-	testParse ("import foo.bar.Baz;");
-	testParse ("import static foo;");
-	testParse ("import static foo.bar;");
-	testParse ("import static foo.bar.Baz;");
-	testParse ("import static foo.bar.Baz.*;");
+	// import foo
+	testParseError (Token.IMPORT, Token.IDENTIFIER);
+	// import foo;
+	testParse (Token.IMPORT, Token.IDENTIFIER, Token.SEMICOLON);
+	// import foo.Bar;
+	testParse (Token.IMPORT, Token.IDENTIFIER, Token.DOT, Token.IDENTIFIER, Token.SEMICOLON);
+	// import foo.*;
+	testParse (Token.IMPORT, Token.IDENTIFIER, Token.DOT, Token.MULTIPLY, Token.SEMICOLON);
+	// import foo.bar.*;
+	testParse (Token.IMPORT, Token.IDENTIFIER, Token.DOT, Token.IDENTIFIER, Token.DOT,
+		   Token.MULTIPLY, Token.SEMICOLON);
+	// import foo.bar.Baz;
+	testParse (Token.IMPORT, Token.IDENTIFIER, Token.DOT, Token.IDENTIFIER, Token.DOT,
+		   Token.IDENTIFIER, Token.SEMICOLON);
+	// import static foo;
+	testParse (Token.IMPORT, Token.STATIC, Token.IDENTIFIER, Token.SEMICOLON);
+	// import static foo.bar;
+	testParse (Token.IMPORT, Token.STATIC, Token.IDENTIFIER, Token.DOT,
+		   Token.IDENTIFIER, Token.SEMICOLON);
+	// import static foo.bar.Baz;
+	testParse (Token.IMPORT, Token.STATIC, Token.IDENTIFIER, Token.DOT, Token.IDENTIFIER,
+		   Token.DOT, Token.IDENTIFIER, Token.SEMICOLON);
+	// import static foo.bar.Baz.*;
+	testParse (Token.IMPORT, Token.STATIC, Token.IDENTIFIER, Token.DOT, Token.IDENTIFIER,
+		   Token.DOT, Token.IDENTIFIER, Token.DOT, Token.MULTIPLY, Token.SEMICOLON);
     }
 
-    private void testParse (String text) {
-	Parser p = getParser (text);
+    private void testParse (Token... tokens) {
+	Parser p = getParser (tokens);
 	p.parse ();
 	checkNoErrors (p);
     }
 
-    private void testParseError (String text) {
-	Parser p = getParser (text);
+    private void testParseError (Token... tokens) {
+	Parser p = getParser (tokens);
 	p.parse ();
 	assert p.getDiagnostics ().hasError ();
     }
 
-    private Parser getParser (String text) {
-	CharBuffer cb = CharBuffer.wrap (text.toCharArray ());
-	Path path = Paths.get ("TestParser");
-	Lexer lexer = new CharBufferLexer (path, cb);
+    private Parser getParser (Token... tokens) {
+	Lexer lexer = new ListLexer (tokens);
 	CompilerDiagnosticCollector diagnostics = new CompilerDiagnosticCollector ();
+	Path path = Paths.get ("TestParser");
 	return new Parser (path, lexer, diagnostics);
     }
 
@@ -77,5 +101,69 @@ public class TestParser {
     private void append (StringBuilder sb, String msg) {
 	sb.append (msg);
 	sb.append ("\n");
+    }
+
+    private static class ListLexer implements Lexer {
+	private final Iterator<Token> tokens;
+
+	public ListLexer (Token... tokens) {
+	    this.tokens = Arrays.asList (tokens).iterator ();
+	}
+
+	public String getError () {
+	    throw new RuntimeException ("No error should appear");
+	}
+
+	public char getCharValue () {
+	    return 'c';
+	}
+
+	public String getStringValue () {
+	    return "SomeString";
+	}
+
+	public int getIntValue () {
+	    return 42;
+	}
+
+	public long getLongValue () {
+	    return 1729L;
+	}
+
+	public float getFloatValue () {
+	    return 3.14f;
+	}
+
+	public double getDoubleValue () {
+	    return 10.0;
+	}
+
+	public long getTokenStartPos () {
+	    return 0;
+	}
+
+	public long getTokenEndPos () {
+	    return 0;
+	}
+
+	public long getLineNumber () {
+	    return 1;
+	}
+
+	public long getTokenColumn () {
+	    return 1;
+	}
+
+	public boolean hasMoreTokens () {
+	    return tokens.hasNext ();
+	}
+
+	public Token nextToken () {
+	    return tokens.next ();
+	}
+
+	public void setInsideTypeContext (boolean insideTypeContext) {
+	    // ignore
+	}
     }
 }
