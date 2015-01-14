35a1a1dc3ebeb9ab49d2738328e7746fc588bd6f
Robert Olofsson <robo@khelekore.org>
2015-01-15 00:01:10 +0100

    Extract the utility class MultiIterator so that it is possible to use it in more places.

--- src/org/khelekore/parjac/parser/MergedMultiState.java
+++ src/org/khelekore/parjac/parser/MergedMultiState.java
@@ -1,6 +1,5 @@
 package org.khelekore.parjac.parser;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -8,6 +7,7 @@ import java.util.Set;
 
 import org.khelekore.parjac.grammar.Rule;
 import org.khelekore.parjac.lexer.Token;
+import org.khelekore.parjac.util.MultiIterator;
 
 class MergedMultiState implements MultiState {
 
@@ -82,32 +82,5 @@ class MergedMultiState implements MultiState {
 	if (ms == null)
 	    return Collections.emptyIterator ();
 	return ms.getRulesWithNext (t);
-    }
-
-    private static class MultiIterator<T> implements Iterator<T> {
-	private Iterator<Iterator<T>> is;
-	private Iterator<T> current;
-
-	public MultiIterator (Iterator<T> i1, Iterator<T> i2, Iterator<T> i3) {
-	    is = Arrays.asList (i1, i2, i3).iterator ();
-	    current = is.next ();
-	    while (!current.hasNext () && is.hasNext ())
-		current = is.next ();
-	}
-
-	public boolean hasNext () {
-	    if (current.hasNext ())
-		return true;
-	    while (is.hasNext ()) {
-		current = is.next ();
-		if (current.hasNext ())
-		    return true;
-	    }
-	    return current.hasNext ();
-	}
-
-	public T next () {
-	    return current.next ();
-	}
     }
 }
