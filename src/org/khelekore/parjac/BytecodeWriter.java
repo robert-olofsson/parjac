4658da60fac58c795feebea25a09a16bdde93194
Robert Olofsson <robo@khelekore.org>
2015-07-19 22:23:07 +0200

    Describe methods.

--- src/org/khelekore/parjac/BytecodeWriter.java
+++ src/org/khelekore/parjac/BytecodeWriter.java
@@ -4,6 +4,12 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 public interface BytecodeWriter {
+    /** Create the output directory needed for some class */
     void createDirectory (Path path) throws IOException;
+
+    /** Write the given bytecode
+     * @Param path the relative path
+     * @Param data the actual bytecode
+     */
     void write (Path path, byte[] data) throws IOException;
 }
